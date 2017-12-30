17692d83819154b716b7bbdd6bbc3c77a1886211
Jaime Pajuelo <jaimepk27@gmail.com>
2017-12-30 18:47:58 +0100

    Load properly kidney-fail.csv

---
+++ parse-kidney-fail.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# =======================================================================================
+# DEPENDENCIES
+# =======================================================================================
+
+import csv
+import json
+import re
+
+# =======================================================================================
+# VARS
+# =======================================================================================
+
+patient_info = [
+  'height',
+  'weight',
+  'urea',
+  'monocytes',
+  'granulocytes',
+  'eosinophils',
+  'basophils',
+  'glucose',
+  'platelets',
+  'mean_platelet_volume',
+  'leukocytes',
+  'trgld',
+  'tflr',
+  'kidney failure'
+]
+
+# =======================================================================================
+# METHODS
+# =======================================================================================
+
+def read_csv(filename):
+  values = []
+  with open(filename, 'rb') as csvfile:
+    values = [row for i, row in enumerate(csv.reader(csvfile)) if i != 0]
+  return values
+
+def exportJSON(data, filename):
+  with open(filename, 'w') as outfile:
+    json.dump(data, outfile, indent=2, separators=(',', ': '), sort_keys=True)
+
+def format_float(val):
+  return float("%.2f" % (float(val),))
+
+def clean(key, val):
+  return (key, None if val == '' else format_float(val))
+
+# =======================================================================================
+# MAIN
+# =======================================================================================
+
+patients = read_csv('kidney-fail.csv')
+patients = dict((int(row[0]), dict(clean(x, y) for x, y in zip(patient_info, row[1:]))) for row in patients)
+
+exportJSON(patients, 'kidney-fail.json')
